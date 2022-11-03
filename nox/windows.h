7f4905d036a73b90d7f3511dcfbbb0131bbf7285
11blankball <filsamare@gmail.com>
2022-11-03 08:35:43 +0700

    Reorganized windows.h

--- nox/windows.h
+++ nox/windows.h
@@ -2,10 +2,6 @@
 #ifndef __windows_h__
 #define __windows_h__
 
-#ifndef NULL
-#define NULL ((void*)0)
-#endif
-
 typedef char BYTE;
 
 typedef unsigned long ULONG;
@@ -30,30 +26,22 @@ typedef const char* LPCSTR;
 typedef void* PVOID;
 typedef const void* LPVOID;
 
+#ifndef NULL
+#define NULL ((void*)0)
+#endif
+
 #define WINAPI __stdcall
 
 #define ERROR_ALREADY_EXISTS 183
 #define INVALID_HANDLE_VALUE (HANDLE)(6)
-DWORD WINAPI GetLastError();
 
-void WINAPI ExitProcess(UINT);
-
-void WINAPI CloseHandle(HANDLE);
 
 #define STD_INPUT_HANDLE ((DWORD)-10)
 #define STD_OUTPUT_HANDLE ((DWORD)-11)
 #define STD_ERROR_HANDLE ((DWORD)-12)
-HANDLE WINAPI GetStdHandle(DWORD);
 
 #define ENABLE_ECHO_INPUT 0x0004
 #define ENABLE_LINE_INPUT 0x0002
-BOOL WINAPI SetConsoleMode(HANDLE, DWORD);
-
-typedef struct _SECURITY_ATTRIBUTES {
-    DWORD nLength;
-    LPVOID lpSecurityDescriptor;
-    WINBOOL bInheritHandle;
-  } SECURITY_ATTRIBUTES, *PSECURITY_ATTRIBUTES, *LPSECURITY_ATTRIBUTES;
 
 #define FILE_SHARE_READ 1
 #define FILE_SHARE_WRITE 2
@@ -63,8 +51,15 @@ typedef struct _SECURITY_ATTRIBUTES {
 
 #define FILE_ATTRIBUTE_NORMAL 0x80
 
-HANDLE WINAPI CreateFileA(LPCSTR, DWORD, DWORD, LPSECURITY_ATTRIBUTES, DWORD, DWORD, HANDLE);
-BOOL WINAPI DeleteFileA(LPCSTR);
+#define FILE_BEGIN 0
+#define FILE_CURRENT 1
+#define FILE_END 2
+
+typedef struct _SECURITY_ATTRIBUTES {
+    DWORD nLength;
+    LPVOID lpSecurityDescriptor;
+    WINBOOL bInheritHandle;
+  } SECURITY_ATTRIBUTES, *PSECURITY_ATTRIBUTES, *LPSECURITY_ATTRIBUTES;
 
 typedef union _LARGE_INTEGER {
     struct {
@@ -80,17 +75,6 @@ typedef union _LARGE_INTEGER {
 
   typedef LARGE_INTEGER *PLARGE_INTEGER;
 
-BOOL WINAPI GetFileSizeEx(HANDLE, PLARGE_INTEGER);
-
-#define FILE_BEGIN 0
-#define FILE_CURRENT 1
-#define FILE_END 2
-BOOL WINAPI SetFilePointerEx(HANDLE, LARGE_INTEGER, PLARGE_INTEGER, DWORD);
-
-BOOL WINAPI ReadConsoleA(HANDLE, LPVOID, DWORD, LPDWORD, LPVOID);
-
-BOOL WINAPI MoveFileA(LPCSTR, LPCSTR);
-
 typedef struct _OVERLAPPED {
     ULONG_PTR Internal;
     ULONG_PTR InternalHigh;
@@ -104,8 +88,22 @@ typedef struct _OVERLAPPED {
     HANDLE hEvent;
   } OVERLAPPED, *LPOVERLAPPED;
 
-BOOL WINAPI ReadFile(HANDLE, LPVOID, DWORD, LPDWORD, LPOVERLAPPED);
+DWORD WINAPI GetLastError();
+void WINAPI ExitProcess(UINT);
+void WINAPI CloseHandle(HANDLE);
 
+HANDLE WINAPI GetStdHandle(DWORD);
+BOOL WINAPI SetConsoleMode(HANDLE, DWORD);
+
+HANDLE WINAPI CreateFileA(LPCSTR, DWORD, DWORD, LPSECURITY_ATTRIBUTES, DWORD, DWORD, HANDLE);
+BOOL WINAPI DeleteFileA(LPCSTR);
+
+BOOL WINAPI GetFileSizeEx(HANDLE, PLARGE_INTEGER);
+BOOL WINAPI MoveFileA(LPCSTR, LPCSTR);
+BOOL WINAPI ReadFile(HANDLE, LPVOID, DWORD, LPDWORD, LPOVERLAPPED);
 BOOL WINAPI WriteFile(HANDLE, LPVOID, DWORD, LPDWORD, LPOVERLAPPED);
+BOOL WINAPI SetFilePointerEx(HANDLE, LARGE_INTEGER, PLARGE_INTEGER, DWORD);
+
+BOOL WINAPI ReadConsoleA(HANDLE, LPVOID, DWORD, LPDWORD, LPVOID);
 
 #endif
